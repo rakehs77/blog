17d4c68f79966538d2f48552fa1fed27dc6e609e
Rakesh <77.rakehs@gmail.com>
2017-03-05 10:28:34 +0545

    updated contact form

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,6 @@
 from django.forms import Form, CharField, Textarea, TextInput
 
 class ContactForm(Form):
-    fullname = CharField(max_length=32, widget=TextInput(attrs={'type': 'text', 'class': 'form-control', 'id': 'name'}), required=True)
-    email = CharField(max_length=32, widget=TextInput(attrs={'type': 'email', 'class': 'form-control', 'id': 'email'}), required=True)
-    message = CharField(widget=Textarea(attrs={'class': 'form-control', 'id': 'message'}), required=True)
+    fullname = CharField(max_length=32, widget=TextInput(attrs={'type': 'text', 'class': 'form-control', 'id': 'name', 'placeholder': 'Fullname', 'data-validation-required-message': 'Please enter your name.'}), required=True)
+    email = CharField(max_length=32, widget=TextInput(attrs={'type': 'email', 'class': 'form-control', 'id': 'email', 'placeholder': 'Email', 'data-validation-required-message': 'Please enter your email.'}), required=True)
+    message = CharField(widget=Textarea(attrs={'class': 'form-control', 'id': 'message', 'placeholder': 'Message', 'data-validation-required-message': 'Please write your message.'}), required=True)
